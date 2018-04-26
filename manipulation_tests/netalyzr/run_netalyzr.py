2fd612387c1cd0cc1fcf967339f7237b7c150f84
Joe (Bestla work) <git@joedeblasio.com>
2018-04-26 16:33:59 -0700

    pep8 for netalyzr

--- manipulation_tests/netalyzr/run_netalyzr.py
+++ manipulation_tests/netalyzr/run_netalyzr.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
-import csv
 import requests
+
 
 def run_netalyzr():
     result_out = subprocess.Popen(
@@ -13,19 +13,23 @@ def run_netalyzr():
     results_url = std_output.split("results available at:")[-1].strip("\n")
     return results_url
 
+
 def dump_results(url):
-    json_link = url.replace("/summary/","/json/")
+    json_link = url.replace("/summary/", "/json/")
     r = requests.get(json_link)
-    output_json = open(sys.argv[1]+"results.json", 'wb')
+    output_json = open(sys.argv[1] + "results.json", 'wb')
     output_json.write(r.content)
     output_json.close()
     return
 
+
 def main():
     result_url = run_netalyzr()
-    link_file = open(sys.argv[1]+"results_link.txt", 'w')
-    link_file.write(result_url+"\n")
+    link_file = open(sys.argv[1] + "results_link.txt", 'w')
+    link_file.write(result_url + "\n")
     link_file.close()
     dump_results(result_url)
+
+
 if __name__ == "__main__":
     main()
